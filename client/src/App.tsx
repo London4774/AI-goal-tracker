643d600a1d07f58d0b42f7d9d2aa527178511ab1
Islam <islambakitzhanuly@gmail.com>
2025-11-04 09:16:12 +0500

    initial version 0.1.0

---
+++ client/src/App.tsx
@@ -0,0 +1,29 @@
+
+
+function App() {
+
+  return (
+    <>
+
+    <h1 className="text-5x1 font-bold underline">
+      Hello, World!
+    </h1>
+
+    <div className="bg-blue-500 text-white">
+      text
+    </div>
+
+    <div className="grid grid-cols-4 gap-3">
+      <div className="p-4 bg-blue-400">Element</div>
+      <div className="p-4 bg-red-400">Element</div>
+      <div className="p-4 bg-yellow-400">Element</div>
+      <div className="p-4 bg-green-400">Element</div>
+      <div className="p-14 bg-red-700 text-white">Element 111</div>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App;
