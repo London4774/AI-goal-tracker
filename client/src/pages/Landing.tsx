907960732fca1ead16c4c063e5ec4865c9f6cdb2
Islam <islambakitzhanuly@gmail.com>
2025-11-23 02:05:42 +0500

    feat: add login/register pages and some fixes landing

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -4,6 +4,7 @@ import Features from "../components/Features";
 import About from "../components/About";
 import Footer from "../components/Footer";
 
+
 function Landing() {
   return (
     <>
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Login(){
+    <>
+        <div>
+            <span>UserName or Email:</span>
+                <input type="text" />
+                <span>Password:</span>
+                <input type="text" />
+                <a href="#">Forgot a password?</a>
+            <a href="#">Dont have a registered?</a>
+        </div>
+    </>
+}
